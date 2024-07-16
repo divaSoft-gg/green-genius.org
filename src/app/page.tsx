02faf26e94516bda4cb351213afd609ec65a6907
Firas Dahmani <firas1dahmani@gmail.com>
2024-07-16 15:35:12 +0200

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 "use client"
-import About from "@/pages/Home/about";
-import CTA from "@/pages/Home/cta";
-import FAQPage from "@/pages/Home/faq";
-import Feature from "@/pages/Home/feature";
-import Hero from "@/pages/Home/hero";
+import About from "@/views/Home/about";
+import CTA from "@/views/Home/cta";
+import FAQPage from "@/views/Home/faq";
+import Feature from "@/views/Home/feature";
+import Hero from "@/views/Home/hero";
 
 export default function Home() {
   return (
