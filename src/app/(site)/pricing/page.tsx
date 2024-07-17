8580eb3c9be057b491fad1e9755d30d9643696e1
Firas Dahmani <firas1dahmani@gmail.com>
2024-07-17 14:52:29 +0200

    styling update

--- src/app/(site)/pricing/page.tsx
+++ src/app/(site)/pricing/page.tsx
@@ -9,7 +9,7 @@ export default function Pricing() {
   return (
     <main className="relative ">
           <section>
-            <div className="relative bg-white  py-4 px-8 lg:py-16 space-y-8 lg:grid lg:grid-cols-3 sm:gap-6 xl:gap-10 lg:space-y-0">
+            <div className="relative bg-white  py-4 px-16 lg:py-16 space-y-8 lg:grid lg:grid-cols-3 sm:gap-6 xl:gap-10 lg:space-y-0">
                 {pricingPlans.map((plan, index) => (
                     <PricingCard
                         key={index}
