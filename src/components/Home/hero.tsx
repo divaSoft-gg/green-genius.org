e7d1699ebd0a3b96724cd11cc72735e0bd4551c5
Firas Dahmani <firas1dahmani@gmail.com>
2024-07-19 13:14:16 +0200

    no impo

--- src/components/Home/hero.tsx
+++ src/components/Home/hero.tsx
@@ -2,32 +2,7 @@ import ButtonCTA from "./atoms/Button";
 
 export default function Hero() {
     return (
-        // <section className="relative isolate px-6 py-20  lg:px-8 ">
-        //     <div className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80 h-[700px] sm:h-[700px]"
-        //         aria-hidden="true">
-        //         <div
-        //             className="relative left-[calc(50%-11rem)] sm:left-[calc(50%+30rem)] aspect-[1155/678] w-[32.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ECCBDE] via-[#FDE3BE] to-[#FBD7B7] opacity-70 sm:w-[72.1875rem]" />
-        //     </div>
-
-        //     <div className="grid max-w-screen-xl px-4 py-2 mx-auto lg:gap-8 xl:gap-0 lg:py-4 lg:grid-cols-12 justify-center">
-        //         <div className="mr-auto place-self-center lg:col-span-7">
-        //             <h1
-        //                 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl ">
-        //                 Empower Your Business with <span className="text-primary">Sustainable Solutions</span></h1>
-        //             <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl ">
-        //                 Track, manage, and reduce your carbon footprint with Green Genius.</p>
-        //             <ButtonCTA />
-        //         </div>
-        //         <div className="mt-4 md:mt-6 lg:mt-12 lg:col-span-5 lg:flex h-96 ">
-        //             <img src="/carbon-footprint-icon.png" className="h-full w-full object-contain"
-        //                 alt="Carbon Footprint Icon" />
-        //         </div>
-        //     </div>
-        // </section>
         <section className="relative bg-[url('https://pagedone.io/asset/uploads/1691055810.png')] bg-center bg-cover  text-black overflow-hidden">
-            {/* <div className="absolute inset-0 bg-primary opacity-50"></div> */}
-            
-            
             <div className="container mx-auto px-4 py-24 md:py-32 relative z-10">
                 <div className="flex flex-col md:flex-row items-center justify-between">
                     {/* <!-- Left Side: Company Info --> */}
@@ -35,7 +10,7 @@ export default function Hero() {
                         <h1 className="text-5xl md:text-6xl font-bold mb-6 leading-tight">
                             Innovate.<br />Transform.<br />Succeed.
                         </h1>
-                        <p className="text-xl mb-8 text-gray-400">Track, manage, and reduce your carbon footprint with Green Genius.</p>
+                        <p className="text-xl mb-8 text-gray-500">Track, manage, and reduce your carbon footprint with Green Genius.</p>
                         <ButtonCTA ClassName={"bg-primary text-white"} />
                         
                     </div>
