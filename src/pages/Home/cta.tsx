6243cf1ca6dd2918ba7b535392565ffa8485d03b
Sarra <sarra.bouazzi2002@gmail.com>
2024-07-22 09:49:13 +0100

    .
    changement du design au niveau de la partie de 'call to action'

--- src/pages/Home/cta.tsx
+++ src/pages/Home/cta.tsx
@@ -1,18 +1,35 @@
 import ArrowLeft from "@/components/icon/ArrowLeft";
 import Link from "next/link";
+import Image from 'next/image';
 
 export default function CTA() {
     return (
-        <section className="bg-white dark:bg-gray-900  ">
-
-            <div className="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
-                <div className="mx-auto max-w-screen-sm text-center">
-                    <h2 className="mb-4 text-4xl tracking-tight font-extrabold leading-tight text-gray-900 dark:text-white">Ready to Make a Difference?</h2>
-                    <p className="mb-6 font-light text-gray-500 dark:text-gray-400 md:text-lg">Join the growing number of businesses committed to sustainability.</p>
+        <section className="relative flex p-8">
+            <div className="w-1/2 bg-black flex items-center justify-center text-left text-white p-8">
+                <div className="max-w-md">
+                    <h2 className="mb-4 text-4xl font-extrabold leading-tight">
+                    Ready to Make a Difference ?
+                    </h2>
+                    <p className="mb-6 text-lg text-gray-400">
+                    Join the growing number of businesses committed to sustainability.
+                    </p>
+                </div>
+            </div>
+            <div className="w-1/2 relative flex items-center justify-center bg-black p-8">
+                <div className="absolute inset-0">
+                    <Image 
+                        src="/65dfd3b99c42d04f7d969a47_Frame40406 (1) (1).png" 
+                        alt="Background Grid" 
+                        layout="fill" 
+                        objectFit="cover" 
+                        quality={100}
+                    />
+                </div>
+                <div className="relative z-10">
                     <Link href="/contact"
-                        className="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-primary hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
-                        Contact Our Team Now
-                        <ArrowLeft />
+                        className="inline-flex items-center justify-center px-6 py-3 text-base font-medium text-black bg-green-500 rounded-lg hover:bg-green-600 focus:ring-4 focus:ring-green-300">
+                        Contact Our Team Now 
+                        <ArrowLeft className="ml-2" />
                     </Link>
                 </div>
             </div>
