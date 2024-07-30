24f0fcf0fa696467a90c7e18ee260ad8f6ea1718
Firas Dahmani <firas1dahmani@gmail.com>
2024-07-30 15:38:58 +0200

    navbar setup

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import Home from './components/pages/Home'
 import Layout from './components/layouts'
+import NotFound from './components/common/NotFound'
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
+        <Route path="*" element={<NotFound />} />
       </Route>
     </Routes>
   )
