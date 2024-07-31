516e4c68e786ec62573ce1ee523f66be2dfbfdc7
aymkh <aymen4real@gmail.com>
2024-07-31 23:29:17 +0100

    fix: using Tailwind theme to configure custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,16 +8,22 @@ export default {
     extend: {
       colors: {
         primary: {
-          50: '#eff6ff',  
+          50: '#eff6ff',
           100: '#dbeafe',
           200: '#bfdbfe',
           300: '#93c5fd',
           400: '#60a5fa',
-          500: '#3b82f6', 
+          500: '#3b82f6',
           600: '#2563eb',
           700: '#1d4ed8',
           800: '#1e40af',
-          900: '#1e3a8a',  
+          900: '#1e3a8a',
+        },
+        'text': {
+          'primary': '#3b82f6',
+        },
+        'bg': {
+          'primary': '#3b82f6',
         },
       }
     },
