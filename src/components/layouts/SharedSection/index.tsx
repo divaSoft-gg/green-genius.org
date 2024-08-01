3be869ef9802d59f37c52f2814f378ad61265c1e
Firas Dahmani <firas1dahmani@gmail.com>
2024-08-01 15:16:03 +0200

    text footer

--- src/components/layouts/SharedSection/index.tsx
+++ src/components/layouts/SharedSection/index.tsx
@@ -3,7 +3,7 @@ import { SharedSectionProps } from '../../../helpers/types';
 
 const SharedSection: React.FC<SharedSectionProps> = ({ children, className = '' }) => {
     return (
-      <section className={` bg-white py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6 ${className}`}>
+      <section className={` bg-white py-10 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6 ${className}`}>
         {children}
       </section>
     );
