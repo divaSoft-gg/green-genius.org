0b79630bf6e2c8bffc9f0dc459aac4a9d740b74d
hellcat29A <bhiri22@gmail.com>
2024-10-29 10:06:35 +0100

    fix:remove-padding

--- src/components/layouts/SharedSection/index.tsx
+++ src/components/layouts/SharedSection/index.tsx
@@ -3,7 +3,7 @@ import { SharedSectionProps } from '../../../helpers/types';
 
 const SharedSection: React.FC<SharedSectionProps> = ({ children, className = '' }) => {
   return (
-    <section className={` bg-white py-10 px-4 mx-auto max-w-screen-xl sm:py-16  ${className}`}>
+    <section className={` bg-white py-10  mx-auto max-w-screen-xl sm:py-16  ${className}`}>
       {children}
     </section>
   );
