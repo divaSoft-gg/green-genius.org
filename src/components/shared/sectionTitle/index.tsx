0af0c2fa43ba0970d30f8c5f04f8f4ecbe95b217
hellcat29A <bhiri22@gmail.com>
2024-11-04 16:52:16 +0100

    feat:features-section

--- src/components/shared/sectionTitle/index.tsx
+++ src/components/shared/sectionTitle/index.tsx
@@ -2,10 +2,10 @@ export default function SectionTitle({ title, subtitle }: Readonly<{ title: stri
 
 
     return (
-        <div>
-            <h1 className="text-4xl font-semibold leading-normal text-left text-black dark:text-white lg:text-6xl">{title}</h1>
+        <div className="my-6">
+            <h1 className="text-4xl font-semibold leading-normal text-left text-black dark:text-white">{title}</h1>
 
-            {subtitle && <p className="mt-2 leading-8 text-white lg:text-lg text-md muted ">{subtitle}</p>}
+            {subtitle && <p className="mt-4 leading-8 text-black dark:text-white lg:text-lg text-md muted ">{subtitle}</p>}
         </div>
     )
 }
