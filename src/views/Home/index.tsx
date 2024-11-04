70dbb00061646d673ff0bbf30bb1b46341d7d875
hellcat29A <bhiri22@gmail.com>
2024-11-04 11:50:25 +0100

    fix:change-home-section

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,9 +1,7 @@
-import HeroSection from "../../components/HeroSection";
 
 export default function Home() {
     return (
         <div className="flex flex-col gap-24">
-            <HeroSection />
         </div>
     )
 }
