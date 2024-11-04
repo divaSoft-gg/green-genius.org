cab4decaf1b3789865fdb6621bff902adcd1a5ab
hellcat29A <bhiri22@gmail.com>
2024-11-04 11:52:40 +0100

    fix:add-hero-section-to-home-page

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,7 +1,9 @@
+import HeroSection from "../../components/HeroSection";
 
 export default function Home() {
     return (
         <div className="flex flex-col gap-24">
+            <HeroSection />
         </div>
     )
 }
