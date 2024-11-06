db5c653d09ecec59e88386eafb0fa6337a690868
hellcat29A <bhiri22@gmail.com>
2024-11-06 13:51:36 +0100

    fix:change-accordion-bg-for-dark-mode

--- src/views/FAQ/index.tsx
+++ src/views/FAQ/index.tsx
@@ -18,7 +18,7 @@ export default function FAQPage() {
                     <div className="flex flex-col items-center gap-8">
                         {questions.map((element: faqType, index: number) => (
                             <Accordion key={index} motionProps={accordionMotionSettings} >
-                                <AccordionItem indicator={<FaChevronDown />} title={<AccordionTitle title={element.question} />} className="px-4 bg-gray-100 border rounded-md">
+                                <AccordionItem indicator={<FaChevronDown />} title={<AccordionTitle title={element.question} />} className="px-4 bg-gray-100 border rounded-md dark:bg-white">
                                     <p className="my-2 leading-relaxed dark:text-black">{element.answer}</p>
                                 </AccordionItem>
                             </Accordion>
@@ -27,7 +27,7 @@ export default function FAQPage() {
                     </div>
                     <div className="flex flex-col gap-8">
                         <h1 className="text-2xl font-bold text-center text-black dark:text-white">{t('faq.sectionFooterTitle')}</h1>
-                        <p className="font-semibold text-center">{t('faq.sectionFooterDescription')}<Link className="font-bold text-black cursor-pointer dark:text-white" href="/contact-us">{t('faq.contactUsLink')}</Link></p>
+                        <p className="font-semibold text-center">{t('faq.sectionFooterDescription')}<Link className="font-bold text-black underline cursor-pointer dark:text-white" href="/contact-us">{t('faq.contactUsLink')}</Link></p>
                     </div>
                 </div>
             </CentredLayout>
